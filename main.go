6646254df2834aab3b24bb2736f37eb22487b617
Vyacheslav Alexeev <alexeev.corp@gmail.com>
2024-06-28 15:45:59 +0200

    hotfix

--- main.go
+++ main.go
@@ -41,77 +41,77 @@ type IOEntry struct {
 
 var inboundApiDownlink = promauto.NewGauge(
         prometheus.GaugeOpts{
-                Name: "xray_inbound_api_downlink",
+                Name: "pp_inbound_api_downlink",
                 Help: "",
         },
 )
 
 var inboundApiUplink = promauto.NewGauge(
         prometheus.GaugeOpts{
-                Name: "xray_inbound_api_uplink",
+                Name: "pp_inbound_api_uplink",
                 Help: "",
         },
 )
 
 var inboundMetricsInDownlink = promauto.NewGauge(
         prometheus.GaugeOpts{
-                Name: "xray_inbound_metrics_in_downlink",
+                Name: "pp_inbound_metrics_in_downlink",
                 Help: "",
         },
 )
 
 var inboundMetricsInUplink = promauto.NewGauge(
         prometheus.GaugeOpts{
-                Name: "xray_inbound_metrics_in_uplink",
+                Name: "pp_inbound_metrics_in_uplink",
                 Help: "",
         },
 )
 
 var inboundVlessTlsDownlink = promauto.NewGauge(
         prometheus.GaugeOpts{
-                Name: "xray_inbound_vless_tls_downlink",
+                Name: "pp_inbound_vless_tls_downlink",
                 Help: "",
         },
 )
 
 var inboundVlessTlsUplink = promauto.NewGauge(
         prometheus.GaugeOpts{
-                Name: "xray_inbound_vless_tls_uplink",
+                Name: "pp_inbound_vless_tls_uplink",
                 Help: "",
         },
 )
 
 var outboundBlockDownlink = promauto.NewGauge(
         prometheus.GaugeOpts{
-                Name: "xray_outbound_block_downlink",
+                Name: "pp_outbound_block_downlink",
                 Help: "",
         },
 )
 
 var outboundBlockUplink = promauto.NewGauge(
         prometheus.GaugeOpts{
-                Name: "xray_outbound_block_uplink",
+                Name: "pp_outbound_block_uplink",
                 Help: "",
         },
 )
 
 var outboundDirectDownlink = promauto.NewGauge(
         prometheus.GaugeOpts{
-                Name: "xray_outbound_direct_downlink",
+                Name: "pp_outbound_direct_downlink",
                 Help: "",
         },
 )
 
 var outboundDirectUplink = promauto.NewGauge(
         prometheus.GaugeOpts{
-                Name: "xray_outbound_direct_uplink",
+                Name: "pp_outbound_direct_uplink",
                 Help: "",
         },
 )
 
 var userDownlink = promauto.NewGaugeVec(
         prometheus.GaugeOpts{
-                Name: "xray_user_downlink",
+                Name: "pp_user_downlink",
                 Help: "",
         },
         []string{"email"},
@@ -119,7 +119,7 @@ var userDownlink = promauto.NewGaugeVec(
 
 var userUplink = promauto.NewGaugeVec(
         prometheus.GaugeOpts{
-                Name: "xray_user_uplink",
+                Name: "pp_user_uplink",
                 Help: "",
         },
         []string{"email"},
@@ -128,10 +128,10 @@ var userUplink = promauto.NewGaugeVec(
 func updateStats(url string, wait int) {
       go func() {
               for {
-                        stats, err := loadXrayStats(url)
+                        stats, err := loadPPStats(url)
 
                         if err != nil {
-                                log.Println("Error polling XRay endpoint, trying again in", wait, "sec")
+                                log.Println("Error polling PP endpoint, trying again in", wait, "sec")
                         } else {
                                 inboundApiDownlink.Set(float64(stats.Inbound.Api.Downlink))
                                 inboundApiUplink.Set(float64(stats.Inbound.Api.Uplink))
@@ -156,7 +156,7 @@ func updateStats(url string, wait int) {
       }()
 }
 
-func loadXrayStats(url string) (stats *StatsRoot, err error) {
+func loadPPStats(url string) (stats *StatsRoot, err error) {
         resp, err := http.Get(url)
 
         if err != nil {
@@ -174,14 +174,14 @@ func loadXrayStats(url string) (stats *StatsRoot, err error) {
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-        w.Write([]byte("Hello to XRay exporter"))
+        w.Write([]byte("Hello to PP exporter"))
 }
 
 func main() {
-        urlPtr := flag.String("url", "", "XRay status endpoint (normally: http://host/debug/vars)")
+        urlPtr := flag.String("url", "", "PP status endpoint (normally: http://host/debug/vars)")
         portPtr := flag.Int("port", 2112, "Port to listen on for metrics")
         endpointPtr := flag.String("endpoint", "/metrics", "Metrics endpoint to listen on")
-        waitPtr := flag.Int("interval", 15, "Interval to update statistics from XRay")
+        waitPtr := flag.Int("interval", 15, "Interval to update statistics from PP")
 
         flag.Parse()
 
@@ -189,7 +189,7 @@ func main() {
           log.Fatalf("Missing required argument -url, see '-help' for information")
         }
 
-        log.Printf("Starting XRay Exporter on :%v%s\n", *portPtr, *endpointPtr)
+        log.Printf("Starting PP Exporter on :%v%s\n", *portPtr, *endpointPtr)
 
         updateStats(*urlPtr, *waitPtr)
 
